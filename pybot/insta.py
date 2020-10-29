925192bec609c356cf840d065192f549baf5577f
Vescik <46301368+Vescik@users.noreply.github.com>
2020-10-29 01:40:44 +0100

    Add files via upload

---
+++ pybot/insta.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from time import sleep
+
+class InstaBot:
+    def __init__(self,userName,password):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.instagram.com/")
+        sleep(3)
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Akceptuję')]")\
+            .click()
+        sleep(3)
+        self.driver.find_element_by_xpath("//input[@name=\"username\"]")\
+            .send_keys(userName)
+        self.driver.find_element_by_xpath("//input[@name=\"password\"]")\
+            .send_keys(password)
+        self.driver.find_element_by_xpath('//button[@type="submit"]')\
+            .click()
+        sleep(4)
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Nie teraz')]")\
+            .click()
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Nie teraz')]")\
+            .click()
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Obserwuj')]")\
+            .click()
+        sleep(20)
+
+
+
+
+InstaBot('DodzioBot69','password')
